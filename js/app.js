377d96b38f3f44bdefc64a541ad0a642e9168a4b
khaingmyozaw <kmyozaw.dev@gmail.com>
2024-04-27 22:45:04 +0630

    mini tech

--- js/app.js
+++ js/app.js
@@ -1,62 +1,9 @@
 
-/* SLIDER SECTION START */
-
-// Slide Forward When next button is clicked
-let slide_items = $('.slider-items');
-let item_length = slide_items.length - 1;
-let dots = $('.dots');
-active = 0;
-
-
-// Auto Slide
-let refreshInterval = setInterval(() => {
-    $('#next').click();
-}, 3000);
-
-// Next button click
-$('#next').click(function() {
-    active = active + 1 <= item_length ? active + 1 : 0;
-    sliderStart();
-});
-
-// Prev button click
-$('#prev').click(function() {
-    active = active - 1 >= 0 ? active - 1 : item_length;
-    sliderStart();
-});
-
-// Dots click
-dots.each( function(index) {
-    $(this).click(function() {
-        active = $(this).index();
-        sliderStart();
-    });
-});
-
-// Function to Start Slider when it is invoked
-function sliderStart() {
-    let activeItem = $('.dots.active');
-
-    $('.slider-list').css({
-        'left': - slide_items[active].offsetLeft + 'px',
-    });
-
-    activeItem.removeClass('active');
-    dots.eq(active).addClass('active');
-
-    clearInterval(refreshInterval);
-    refreshInterval = setInterval(() => {
-        $('#next').click();
-    }, 3000);
-}
-
-/* SLIDER SECTION END */
-
 /* PRODUCTS DEVIDED WITH CATEGORIES */
 
 // All
 $('.list-items button.showAll').click(function() {
-    common($);
+    common($(this));
     $('.product-items').removeClass('d-none');
 });
 
